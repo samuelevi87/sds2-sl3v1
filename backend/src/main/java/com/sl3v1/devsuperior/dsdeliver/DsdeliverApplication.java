12b2cb7f5d19eeb8a6ea122bded72703e02576f8
samuelevi87 <samuel.levi.alves@outlook.com>
2021-01-06 23:29:27 -0300

    Project Created

---
+++ backend/src/main/java/com/sl3v1/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.sl3v1.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
